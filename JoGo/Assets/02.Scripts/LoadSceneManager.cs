58df3ad990a0ab29d76a6e79030b77db49ed676f
deunny <godsatm@gmail.com>
2019-08-06 11:43:24 +0900

    version2_UI
    version2_UI

---
+++ JoGo/Assets/02.Scripts/LoadSceneManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneManager : MonoBehaviour
+{
+    public void LoadStore()
+    {
+        SceneManager.LoadScene("Shop");
+    }
+
+    public void LoadWalk()
+    {
+        SceneManager.LoadScene("scMap");
+    }
+
+    public void LoadSetRoom()
+    {
+        SceneManager.LoadScene("MyRoom");
+    }
+
+    public void LoadInventory()
+    {
+        SceneManager.LoadScene("Inventory");
+    }
+
+    public void LoadMain()
+    {
+        SceneManager.LoadScene("Cat_Interaction");
+    }
+}
