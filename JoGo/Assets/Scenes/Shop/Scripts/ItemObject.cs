b4e7a5e7455f5e303104d9b4f449bfe5b14ff93f
deunny <godsatm@gmail.com>
2019-08-06 11:10:16 +0900

    Version2_Myroom,Store
    Version2_Myroom,Store

---
+++ JoGo/Assets/Scenes/Shop/Scripts/ItemObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemObject : MonoBehaviour
+{
+    public Button Item;
+    public Image Face;
+    public GameObject itemInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        itemInfo.SetActive(true);
+    }
+}
