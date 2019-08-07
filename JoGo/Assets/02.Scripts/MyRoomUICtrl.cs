1b223bd4b7234e9ea9e2803fe4079bc89f2476a1
PigTurtle <49774211+PigTurtle@users.noreply.github.com>
2019-08-08 04:52:06 +0900

    myroom

---
+++ JoGo/Assets/02.Scripts/MyRoomUICtrl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyRoomUICtrl : MonoBehaviour
+{
+    public GameObject FList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FList.SetActive(false);
+    }
+
+    public void OnTouchXButton()
+    {
+
+    }
+
+    public void OnTouchAddFButton()
+    {
+        if (!FList.activeSelf)
+            FList.SetActive(true);
+        else
+            FList.SetActive(false);
+    }
+
+    public void OnTouchSaveButton()
+    {
+
+    }
+
+    public void OnTouchF1()
+    {
+
+    }
+
+    public void OnTouchF2()
+    {
+
+    }
+
+    public void OnTouchF3()
+    {
+
+    }
+
+    public void OnTouchF4()
+    {
+
+    }
+
+}
