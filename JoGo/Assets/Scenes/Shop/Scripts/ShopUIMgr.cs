81abfbf5cffe7e7283aba8d9fe4f90374798ae1d
asdfjkl99 <36942665+asdfjkl99@users.noreply.github.com>
2019-08-27 01:04:00 +0900

    ShopUIMgr update

---
+++ JoGo/Assets/Scenes/Shop/Scripts/ShopUIMgr.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopUIMgr : ItemInfoCls
+{
+    public GameObject itemInfo;
+    public GameObject buyPopUp;
+    public Text itemCount;
+    public Button increase;
+    public Button decrease;
+    int num = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemInfo.SetActive(false);
+        buyPopUp.SetActive(false);
+    }
+
+    void Update()
+    {
+        itemCount.text = num.ToString();
+    }
+
+    public void IncreaseOnClick()
+    {
+        num++;
+    }
+
+    public void DecreaseOnClick()
+    {
+        if (num > 1)
+            num--;
+    }
+}
